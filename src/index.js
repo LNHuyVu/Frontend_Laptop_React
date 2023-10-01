75ee8fe4f57bed849fc6ab548d58ab3843ca4a49
LNHuyVu <lenguyenhuyvu.htn@gmail.com>
2023-10-01 20:46:06 +0700

    Fix size file

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ import { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
